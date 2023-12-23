8b52837ffa2220b1a90126a0cdcb339b1a68dabb
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-12-23 23:45:24 +0000

    Update line_follower.c

--- line_follower.c
+++ line_follower.c
@@ -67,8 +67,8 @@ void line_follower(int argc, char *argv[])
 //======================================================================
 int main(int argc, char *argv[])
 {
-  WINDOW *mainwin = initscr();  // curses: init screen
-  noecho();                    // curses: prevent the key being echoed
+  WINDOW *mainwin = initscr();  //curses: init screen
+  noecho();                    //curses: prevent the key being echoed
   cbreak();                    // curses: disable line buffering 
   nodelay(mainwin, TRUE);       // curses: set getch() as non-blocking 
   keypad(mainwin, TRUE);        // curses: enable detection of cursor and other keys
